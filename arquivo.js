9c0b80d30a86917d57013c062c68f608e4221088
JoaquimSchuquel <133259626+JoaquimSchuquel@users.noreply.github.com>
2025-03-19 16:12:51 -0400

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -18,15 +18,17 @@ const download = function () {
     const a = document.createElement("a")
     a.style.display = "none"
     document.body.appendChild(a)
+    return function (conteudo, nomeArquivo) {
+        const blob = new Blob([conteudo], {type: 'application/octet-stream'})
+        const url = window.URl.createObjectURL(blob)
+        a.href = url
+        a.download = nomeArquivo
+        a.click()
+        window.URL.revokerObjectURL(url)
 }
-return function (conteudo, nomeArquivo) {
-    const blob = new Blob([conteudo], {type: 'application/octet-stream'})
-    const url = window.URl.createObjectURL(blob)
-    a.href = url
-    a.download = nomeArquivo
-    a.click()
-    window.URL.revokerObjectURL(url)
+
 }
+
 btndownload.addEventListener('click', function () {
     const conteudo = preview.value
     download()(conteudo, 'texto.txt')
